15ada277f8f67e3997966bf5f1f52f3d1e6421a7
Juan Bautista Correa <jbautistacorrea@gmail.com>
2023-02-23 11:00:53 -0300

    Quita de datos personales y cambios menores

--- Twitter-Basic-Bot.py
+++ Twitter-Basic-Bot.py
@@ -16,9 +16,9 @@ api = tw.API(auth, wait_on_rate_limit=True)
 #Comentadas para que no de errores
 
 #Buscar usuario (puede ser por id o screen_name, o por id de tweet)
-#user = api.get_user(id=)
-#print=(user.name)
-#print(user.description)
+user = api.get_user(id='')
+print=(user.name)
+print(user.description)
 
 #Seguir usuario
 api.create_friendship(screen_name='generic_name')    
@@ -28,26 +28,26 @@ api.update_status("Generic tweet")
 
 
 #Likear 10 tweets de la timeline
-#tweets_home= api.home_timeline(count=10)
-#for tweet in tweets_home:
-#    if tweet.author.name.lower() != 'generic_name':
-#        if not tweet.favorited:
-#            print(f"Liking this tweet from ({tweet.author.name})")
-#            api.create_favorite(tweet.id)
+tweets_home= api.home_timeline(count=10)
+for tweet in tweets_home:
+    if tweet.author.name.lower() != 'generic_name':
+        if not tweet.favorited:
+            print(f"Liking this tweet from ({tweet.author.name})")
+            api.create_favorite(tweet.id)
             
 
 #Buscar usuario y likear ultimos tweets
-#user= api.get_user(screen_name='generic_name')
-#user_tweets=api.user_timeline(user_id=user.id)
-#for tweet in user_tweets:
-#    if not tweet.favorited:
-#       api.create_favorite(tweet.id)  
+user= api.get_user(screen_name='generic_name')
+user_tweets=api.user_timeline(user_id=user.id)
+for tweet in user_tweets:
+    if not tweet.favorited:
+       api.create_favorite(tweet.id)  
 
 
 #Busca e imprime por contenido de tweet
-#tweets=api.search_tweets( q='generic_search', count=10)
-#for tweet in tweets:
-#    print(tweet.text)    
+tweets=api.search_tweets( q='generic_search', count=10)
+for tweet in tweets:
+    print(tweet.text)    
             
             
             
